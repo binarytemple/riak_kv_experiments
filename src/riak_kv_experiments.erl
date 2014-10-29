48c1c922eba38ecc5c2fb369e04f4b7370d4a375
bryan hunt <github@binarytemple.co.uk>
2014-10-29 00:18:40 +0000

    leveldb outside riak

---
+++ src/riak_kv_experiments.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author bryanhunt
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 27. Oct 2014 20:21
+%%%-------------------------------------------------------------------
+-module(riak_kv_experiments).
+-author("bryanhunt").
+
+%% API
+-export([foo/0]).
+
+foo() -> ok.
---
+++ src/riak_kv_poc.erl
@@ -0,0 +1,12 @@
+-module(riak_kv_poc).
+
+-export([hello/0]).
+
+hello() ->
+  application:set_env(riak_kv, storage_backend, bitcask),
+%%   app_helper:set_env(riak_kv, storage_backend, bitcask),
+  {ok, Pid} = riak_kv_app:start(foo,bar),
+
+
+ ok.
+
