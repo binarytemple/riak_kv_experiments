9d03d0d58e2ae06d3e018824e270f14b38f3b6d5
bryan hunt <github@binarytemple.co.uk>
2014-10-30 00:11:20 +0000

    working example

--- src/riak_kv_eleveldb_backend_poc.erl
+++ src/riak_kv_eleveldb_backend_poc.erl
@@ -6,47 +6,59 @@
 %% {<<"field_bin">>, <<"A">>},
 %% {<<"field_int">>, 1}
 %% ]
-create_object(Bucket, Key, Val, MetaDataList) ->
-  io:format("create_object ~p ~p ~p ~p ~n", [Bucket, Key, Val, MetaDataList]),
-%%   dict:new()
-  riak_object:new(Bucket, Key, Val, dict:new())
-%%  riak_object:new(Bucket, Key, Val, dict:from_list([{?MD_INDEX, MetaDataList}]) )
-.
 
-die(Reason) ->
-  erlang:exit(Reason)
+
+%%
+-type index_spec() :: {binary(), (binary() | integer())}.
+-spec create_object(riak_object:bucket(), riak_object:key(), term(), [index_spec()]) -> {ok, term()}.
+create_object(Bucket, Key, Val, IndexData) ->
+  io:format("create_object ~p ~p ~p ~p ~n", [Bucket, Key, Val, IndexData]),
+  Obj = riak_object:new(Bucket, Key, Val),
+  io:format("Obj ~p ~n", [Obj]),
+  Obj1 = riak_object:update_metadata(Obj,
+    dict:store(<<"index">>, IndexData,
+      dict:new())),
+  {ok, riak_object:apply_updates(Obj1)}
 .
 
 init() ->
   io:format("keys_example leveldb ~n", []),
-%%   Obj  = create_object(<<"a">>,<<"b">>,<<"bar">>,  [{<<"field_bin">>, <<"A">>}]),
-  Obj = create_object(<<"a">>, <<"b">>, <<"bar">>, []),
-  io:format("object ~p ~n", [Obj]),
 
-%%   app_helper:  get_prop_or_env(data_root, Config, eleveldb)
+  application:load(eleveldb),
 
-
-  %% This is where everything breaks ;-)
   {ok, State} = riak_kv_eleveldb_backend:start(0, [
-    {data_root, "ltest2"}
+    {max_open_files, 100},
+    {compression, false},
+    {data_root, "itest2"}
   ]),
+
+  Big = lists:foldl(fun(I, Acc) -> [round(random:uniform() * 100) * I | Acc] end, [], lists:seq(1, 100)),
+
+  io:format("Big ~p", [Big]),
+  io:format("backend_status: ~p ~n", [riak_kv_eleveldb_backend:status(State)]),
+  io:format("backend is empty : ~p ~n", [riak_kv_eleveldb_backend:is_empty(State)]),
   io:format("state: ~p ~n", [State]),
   try
     io:format("in the block  ~n", []),
-%% riak_kv_eleveldb_backend:put(<<"foo">>, <<"bar">>, [{add, {<<"field_int">>, 1}}], <<"dsadfa">>, State),
-%%    riak_kv_eleveldb_backend:put(<<"a">>, <<"b">>, [], <<"asdfa">>, State),
-    riak_kv_eleveldb_backend:put(<<"a">>, <<"b">>, [], Obj, State),
-%%     {ok, D, _} = riak_kv_eleveldb_backend:get(<<"foo">>, <<"bar">>, State),
-%%     io:format("retrieved ~p  ~n", [D]),
-%%     Keys = riak_kv_eleveldb_backend:fold_keys(fun(X, Y, Z) ->
-%%       io:format("folded over ~p ~p ~p ~n", [X, Y, Z]),
-%%       [] end, [], [], State),
+    {ok, Obj} = create_object(<<"foo">>, <<"bar">>, Big, [{<<"name_bin">>, <<"dogs">>}, {<<"num_int">>, 1}]),
+    {ok, NewState} =
+      riak_kv_eleveldb_backend:put(
+        <<"test">>, <<"test">>,
+        riak_object:index_specs(Obj),
+        riak_object:to_binary(v1, Obj), State),
+
+    io:format("new state: ~p ~n", [NewState]),
     io:format("finished the block  ~n", [])
   catch
-    _:E -> io:format("caught ~p ", [E])
+    E:F -> io:format("[error]: caught ~p ~p ", [E, F])
   after
     riak_kv_eleveldb_backend:stop(State)
   end,
   io:format("finished playtime with leveldb ~n", []),
-%%   timer:apply_after(1, leveldb_poc, die, ["finished"]),
+
+  timer:apply_after(1, leveldb_poc, die, ["finished"]),
   ok.
+
+die(Reason) ->
+  erlang:exit(Reason)
+.
